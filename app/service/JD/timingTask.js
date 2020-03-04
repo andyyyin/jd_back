1ff3c8d5932c36a0cc1dea94eff2139dcfcc9c5b
Andy <ycwd123@163.com>
2020-03-05 04:07:39 +0800

    添加每天0点10分补充查询任务

--- app/service/JD/timingTask.js
+++ app/service/JD/timingTask.js
@@ -1,22 +1,26 @@
 const {TASK_INTERVAL} = require('./constant')
 
-const TIME_ZONE_OFF = 3600000 // 北京(东8)时区时差值
-
 const MAX_TRY_TIME = 3 // 出错时的最大尝试次数
+const TIME_OFF = -3600000 // 北京(东8)时区时差值
+const DAY_TIME = 1000 * 60 * 60 * 24
 
 const getNextTimeFrom = (time) => {
-  return time + TASK_INTERVAL - ((time - TIME_ZONE_OFF) % TASK_INTERVAL)
+  return time + TASK_INTERVAL - ((time + TIME_OFF) % TASK_INTERVAL)
+}
+const getNextSupplyTimeFrom = (time) => {
+  const timeAfterZero = 1000 * 60 * 10 // 超过10分钟，下面计算结果是每天的0点10分
+  return time - (time % DAY_TIME) + 57600000 + DAY_TIME + timeAfterZero
 }
 
 let timingTask;
 
 const start = (taskRun) => {
-  let nextTime = 0
+  let nextTime = 0;
+  let nextSupplyTime = getNextSupplyTimeFrom(Date.now())
   let runningFlag = false
 
   const dealTask = async () => {
     runningFlag = true
-    nextTime = getNextTimeFrom(Date.now())
     let tryTime = 0;
     while (tryTime++ <  MAX_TRY_TIME) {
       try {
@@ -34,8 +38,17 @@ const start = (taskRun) => {
 
   timingTask = setInterval(() => {
     if (runningFlag) return
-    if (Date.now() > nextTime) {
+    const now = Date.now()
+    if (now > nextTime) {
+      nextTime = getNextTimeFrom(now)
       dealTask().then()
+    }
+    /* 每天0点10分补充查询 */
+    if (now > nextSupplyTime) {
+      nextSupplyTime = getNextSupplyTimeFrom(now)
+      if (!runningFlag) {
+        dealTask().then()
+      }
     }
   }, 666)
 }
